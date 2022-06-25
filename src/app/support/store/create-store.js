ca911cf16719720b758b99aecc2b933f714a6c41
Nguyen Tuan Linh <linhnt.aim@outlook.com>
2022-06-25 21:57:08 +0700

    store

--- src/app/support/store/create-store.js
+++ src/app/support/store/create-store.js
@@ -1,5 +1,13 @@
 import {createStore as baseCreateStore} from 'vuex'
+import {registerPropertyFactory, take} from '../helpers'
 
 export function createStore(options = {}) {
-    return baseCreateStore(options)
+    return take(baseCreateStore(options), store => {
+        const install = store.install
+        store.install = function (app, injectKey) {
+            install.call(this, app, injectKey)
+
+            registerPropertyFactory(this, 'app', () => app._instance.proxy)
+        }
+    })
 }
